6656d53e8211510024bbfbf1f02277957659ac4b
kingkmademax <kirubelmamushet@gmail.com>
2026-02-19 09:07:21 +0300

    dash  bord

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,13 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import * as schema from "@/db/schema"; // your drizzle schema
 export const auth = betterAuth({
+        socialProviders: {
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    },
+
     emailAndPassword: {
     	enabled: true,
     	autoSignIn: false //defaults to true
